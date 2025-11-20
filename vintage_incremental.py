f3e3e94ade46e3aab30f42b93c7a5033e1ed32bf
TheRealCthulhu <itsmetherealcthulhu@gmail.com>
2025-11-19 21:07:36 -0500

    Creation of Files

---
+++ vintage_incremental.py
@@ -0,0 +1,21 @@
+"""
+Cameron Lanphere
+Asa Rousseau
+Final Project - Vintage Incremental
+"""
+
+
+# Imports
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+import time
+
+
+# Function Definitions
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+
+    root.mainloop()
